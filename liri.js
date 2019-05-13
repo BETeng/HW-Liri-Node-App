acb68fabd5c028b786a7f359ed1ac8c3d068b2f5
BETeng <brian7.teng@gmail.com>
2019-05-13 11:01:27 -0500

    iniitial comit

---
+++ liri.js
@@ -0,0 +1,19 @@
+// CODE FOR LIRI
+
+
+
+// set up process.argv[2] to recognize 
+  // concert-this
+  // spotify-this-song
+  // movie-this
+  // do-what-it-says
+
+//API key and set up for bootcamp
+//"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+//get name/ of band, venue location, and date 
+  //use moment to format date ouptut from API
+
+  // var axios = require("axios")
+  // var Spotify = require('node-spotify-api');
+
+  console.log(process.argv);

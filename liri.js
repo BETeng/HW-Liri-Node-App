14b82038d340a47113f53b9cd8ad3341a64cd161
BETeng <brian7.teng@gmail.com>
2019-05-14 23:50:20 -0500

    setup keys and spotify

--- liri.js
+++ liri.js
@@ -12,8 +12,33 @@
 //"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
 //get name/ of band, venue location, and date 
   //use moment to format date ouptut from API
-
-  // var axios = require("axios")
-  // var Spotify = require('node-spotify-api');
-
+  require("dotenv").config();
+  var keys = require("./keys.js");
+  const Spotify = require('node-spotify-api');
+  var spotify = new Spotify(keys.spotify);
+  const axios = require("axios")
+  
+  // const db = require('db')
   console.log(process.argv);
+ 
+  spotify
+    .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+    .then(function(data) {
+      console.log(data); 
+    })
+    .catch(function(err) {
+      console.error('Error occurred: ' + err); 
+    });
+
+
+
+
+
+
+
+//     ---- OMDB AXIOS CALL
+//   axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+//   function(response) {
+//     console.log("The movie's rating is: " + response.data.imdbRating);
+//   }
+// );

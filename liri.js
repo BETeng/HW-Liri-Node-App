0fac075e53dacc46f6bf03101814039bd131dfa7
BETeng <brian7.teng@gmail.com>
2019-05-17 01:43:52 -0500

    finished omdb api call

--- liri.js
+++ liri.js
@@ -19,26 +19,26 @@ var spotify = new Spotify(keys.spotify);
 const axios = require("axios")
 // const db = require('db')
 
-  // if (process.argv[2] == 'spotify-this-song'){
-  //   const trackInput = process.argv[3] 
-  //   spotify
-  //   .search({
-  //     type: 'track',
-  //     query: trackInput,
-  //     limit: 1
-  //   })
-  //   .then(function (response) {
-  //     let artistName = response.tracks.items[0].album.artists[0].name
-  //     console.log('artist(s):',artistName);
-  //     let album = response.tracks.items[0].album.name
-  //     console.log('album:',album)
-  //     let songName = response.tracks.items[0].name
-  //     console.log('song:',songName)
-  //   })
-  //   .catch(function (err) {
-  //     console.error('Error occurred: ' + err);
-  //   });
-  // }
+// if (process.argv[2] == 'spotify-this-song'){
+//   const trackInput = process.argv[3] 
+//   spotify
+//   .search({
+//     type: 'track',
+//     query: trackInput,
+//     limit: 1
+//   })
+//   .then(function (response) {
+//     let artistName = response.tracks.items[0].album.artists[0].name
+//     console.log('artist(s):',artistName);
+//     let album = response.tracks.items[0].album.name
+//     console.log('album:',album)
+//     let songName = response.tracks.items[0].name
+//     console.log('song:',songName)
+//   })
+//   .catch(function (err) {
+//     console.error('Error occurred: ' + err);
+//   });
+// }
 
 
 
@@ -46,9 +46,55 @@ const axios = require("axios")
 
 
 
-//     ---- OMDB AXIOS CALL
-//   axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
-//   function(response) {
-//     console.log("The movie's rating is: " + response.data.imdbRating);
-//   }
-// );
+
+// ---- OMDB AXIOS CALL    ***NEED TO SET UP ERROR HANDLING FOR UNRECOGNIZED TITLES
+
+if (process.argv[2] == 'movie-this' && process.argv[3] != undefined) {
+  let title = process.argv[3]
+  axios.get("http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy").then(
+    function (response) {
+      let Title = response.data.Title
+      console.log(Title)
+      let year = response.data.Year
+      console.log(year)
+      let rottenTomatoes = response.data.Ratings[1].Value
+      console.log(rottenTomatoes)
+      let countryProduced = response.data.Country
+      console.log(countryProduced)
+      let language = response.data.Language
+      console.log(language)
+      let plot = response.data.Plot
+      console.log(plot)
+      let actors = response.data.Actors
+      console.log(actors)
+      let imdbRating = response.data.imdbRating
+      console.log(imdbRating);
+    }
+  );
+  
+}
+else {
+  let title = 'Mr. Nobody'
+  axios.get("http://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy").then(
+    function (response) {
+      let Title = response.data.Title
+      console.log(Title)
+      let year = response.data.Year
+      console.log(year)
+      let rottenTomatoes = response.data.Ratings[1].Value
+      console.log(rottenTomatoes)
+      let countryProduced = response.data.Country
+      console.log(countryProduced)
+      let language = response.data.Language
+      console.log(language)
+      let plot = response.data.Plot
+      console.log(plot)
+      let actors = response.data.Actors
+      console.log(actors)
+      let imdbRating = response.data.imdbRating
+      console.log(imdbRating);
+    }
+  );
+}
+
+
